3172ab1e0848d11b61298a9453190dc70d37037f
cafaray <cafaray@gmail.com>
2016-04-02 14:03:37 -0600

    Actualizacion de proyectos

--- pixup-app/src/mx/com/pixup/app/Main.java
+++ pixup-app/src/mx/com/pixup/app/Main.java
@@ -10,25 +10,35 @@ public class Main {
 
     public static void main(String[] args) {
         UsuarioBO bo = new UsuarioBOImpl();
+//        Usuario usuario = new Usuario();
+//        usuario.setNombre("Alberto");
+//        usuario.setApellidoPaterno("Ruiz");
+//        usuario.setApellidoMaterno("Vazquez");
+//        usuario.setEmail("aruizv@gmail.com");
+//        Calendar fechaNacimiento = Calendar.getInstance();
+//        fechaNacimiento.set(Calendar.YEAR, 1979);
+//        fechaNacimiento.set(Calendar.MONTH, 9);
+//        fechaNacimiento.set(Calendar.DATE, 19);
+//        usuario.setFechaNacimiento(fechaNacimiento.getTime());
+//        usuario.setGenero("H");
+//        usuario.setNumeroTelefonico("5560121201");
+//        usuario.setPassword("elPaso");
+//        try{
+//            usuario = bo.registrar(usuario);
+//            System.out.println("Se ingreso el usuario correctamente: "+usuario.getNombre());
+//        }catch(PixUpBOException e){
+//            e.printStackTrace(System.out);
+//        }
+        
         Usuario usuario = new Usuario();
-        usuario.setNombre("Alberto");
-        usuario.setApellidoPaterno("Ruiz");
-        usuario.setApellidoMaterno("Vazquez");
-        usuario.setEmail("aruizv@gmail.com");
-        Calendar fechaNacimiento = Calendar.getInstance();
-        fechaNacimiento.set(Calendar.YEAR, 1979);
-        fechaNacimiento.set(Calendar.MONTH, 9);
-        fechaNacimiento.set(Calendar.DATE, 19);
-        usuario.setFechaNacimiento(fechaNacimiento.getTime());
-        usuario.setGenero("H");
-        usuario.setNumeroTelefonico("5560121201");
-        usuario.setPassword("elPaso");
-        try{
-            usuario = bo.registrar(usuario);
-            System.out.println("Se ingreso el usuario correctamente: "+usuario.getNombre());
-        }catch(PixUpBOException e){
-            e.printStackTrace(System.out);
-        }
+                try{
+                    usuario = bo.validaAcceso("aruiz@gmail.com", "elPaso");
+                    System.out.println("usuario: "+usuario.getNombre());
+                }catch(PixUpBOException e){
+                    e.printStackTrace(System.out);
+                } catch(NullPointerException e){
+                    System.out.println("Se encontró un valor nulo, imposible continuar.");
+                }
 
     }
 

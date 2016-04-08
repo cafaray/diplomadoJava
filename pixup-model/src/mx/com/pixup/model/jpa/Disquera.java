9be1b73b03e845e2f8165721b117eeaf23a5b863
Alberto Farias <cfarias@tsmi.com.mx>
2016-04-08 18:11:16 -0500

    Actualización para ejercicio de EJB de sesión con estado

---
+++ pixup-model/src/mx/com/pixup/model/jpa/Disquera.java
@@ -0,0 +1,102 @@
+package mx.com.pixup.model.jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@Table(name = "disquera")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Disquera.findAll", query = "SELECT d FROM Disquera d"),
+    @NamedQuery(name = "Disquera.findById", query = "SELECT d FROM Disquera d WHERE d.id = :id"),
+    @NamedQuery(name = "Disquera.findByNombre", query = "SELECT d FROM Disquera d WHERE d.nombre = :nombre")})
+public class Disquera implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idDisquera", fetch = FetchType.EAGER)
+    private List<Disco> discoList;
+
+    public Disquera() {
+    }
+
+    public Disquera(Integer id) {
+        this.id = id;
+    }
+
+    public Disquera(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public List<Disco> getDiscoList() {
+        return discoList;
+    }
+
+    public void setDiscoList(List<Disco> discoList) {
+        this.discoList = discoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Disquera)) {
+            return false;
+        }
+        Disquera other = (Disquera) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.com.pixup.model.jpa.Disquera[ id=" + id + " ]";
+    }
+    
+}
---
+++ pixup-model/src/mx/com/pixup/model/jpa/Pais.java
@@ -0,0 +1,102 @@
+package mx.com.pixup.model.jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@Table(name = "pais")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pais.findAll", query = "SELECT p FROM Pais p"),
+    @NamedQuery(name = "Pais.findById", query = "SELECT p FROM Pais p WHERE p.id = :id"),
+    @NamedQuery(name = "Pais.findByNombre", query = "SELECT p FROM Pais p WHERE p.nombre = :nombre")})
+public class Pais implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPais", fetch = FetchType.EAGER)
+    private List<Disco> discoList;
+
+    public Pais() {
+    }
+
+    public Pais(Integer id) {
+        this.id = id;
+    }
+
+    public Pais(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public List<Disco> getDiscoList() {
+        return discoList;
+    }
+
+    public void setDiscoList(List<Disco> discoList) {
+        this.discoList = discoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pais)) {
+            return false;
+        }
+        Pais other = (Pais) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.com.pixup.model.jpa.Pais[ id=" + id + " ]";
+    }
+    
+}
